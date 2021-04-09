7af243d47f51ac533955e54c512c4afa6841f6da
daichi-ranguru <langr.ondrej.work@gmail.com>
2021-04-09 11:36:26 +0200

    Updateee

--- entry.py
+++ entry.py
@@ -1,22 +1,29 @@
-import os
+from os import environ, path, walk
+from pathlib import Path
+from urllib.parse import urlparse, unquote
+from sys import argv
 from dotenv import load_dotenv
 import ftplib
 
 def missingValues(name): 
   raise AttributeError("Env '{}' value missing!".format(name))
 
-envir = os.environ
-load_dotenv(".env")
-root = envir.get("UWF_ROOT") if envir.get("UWF_ROOT") is not None else missingValues("root")
-ftpRoot = envir.get("UWF_FTP_FOLDER") if envir.get("UWF_FTP_FOLDER") is not None else "/"
-excludeFolders = set(envir.get("UWF_EXCLUDE").split(",") if envir.get("UWF_EXCLUDE") is not None else [])
+if len(argv) != 2:
+  raise ValueError("Command wrong. Should be like this: 'python <py_script> <root_folder_of_files>'")
+root = Path(argv[1])
+if root.exists() is False:
+  raise ValueError("Specified root folder does not exist.")
+
+load_dotenv("{}/.env".format(root))
+
+excludeFolders = set(environ.get("UWF_EXCLUDE").split(",") if environ.get("UWF_EXCLUDE") is not None else [])
 excludeFiles = ["upload-with-python.py"]
 
-
 ftpConfig = {
-  'server': envir.get("UWF_FTP_SERVER"), 
-  'user': envir.get("UWF_FTP_USER"),
-  'passw': envir.get("UWF_FTP_PASS")
+  'server': environ.get("UWF_FTP_SERVER"), 
+  'user': environ.get("UWF_FTP_USER"),
+  'passw': environ.get("UWF_FTP_PASS"),
+  'root': environ.get("UWF_FTP_FOLDER") if environ.get("UWF_FTP_FOLDER") is not None else "/"
 }
 
 for key in ftpConfig:
@@ -24,19 +31,21 @@ for key in ftpConfig:
     missingValues(key)
 
 session = ftplib.FTP(ftpConfig["server"],ftpConfig["user"],ftpConfig["passw"])
-session.cwd(ftpRoot)
+session.cwd(ftpConfig["root"])
 
 print("Uploading started!")
 
-for dname, dirs, files in os.walk(root, topdown=True):
+for dname, dirs, files in walk(root, topdown=True):
   dirs[:] = [d for d in dirs if d not in excludeFolders] 
   files[:] = [f for f in files if f not in excludeFiles] 
-  curFolder = dname.replace("./", "")
-  print("Current folder: {}".format(curFolder))
+  ndname = Path(dname).as_uri()
 
+  curFolder = ndname.replace(root.as_uri(), "")
+
+  print("Current folder: {}".format(curFolder))
   # change to current working folder
   if curFolder != "":
-    toChange = ftpRoot + "/" + curFolder.replace('\\', '/')
+    toChange = ftpConfig["root"] +  curFolder
     if session.pwd() is not (toChange):
       session.cwd(toChange)
 
@@ -45,12 +54,14 @@ for dname, dirs, files in os.walk(root, topdown=True):
     for dirName in [a for a in dirs if a not in session.nlst()]:
       session.mkd(dirName)
 
+  # store files
   for fname in files:
-    fileName = os.path.join(dname, fname)
+    fileName = path.join(dname, fname)
     session.storbinary(
       'STOR {}'.format(fname), 
       open(fileName, 'rb')
     )
+  
 
 print("Done! Stopping...")
 session.quit()
